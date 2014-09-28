025446858799df67ee67ac4dd49c9a29798c2a13
Grayside <aross@phase2technology.com>
2014-09-27 20:19:05 -0500

    Override all task options with target configuration per property.

--- tasks/redbot.js
+++ tasks/redbot.js
@@ -1,6 +1,36 @@
 'use strict';
 
 module.exports = function(grunt) {
+    /**
+     * Override task "global" options with target-specific configuration.
+     */
+    var targetConfigOverride = function(options, targetConfig) {
+        var config = [];
+        Object.keys(options).forEach(function(attr) {
+            config[attr] = targetConfig[attr] ? targetConfig[attr] : options[attr];
+        });
+
+        return config;
+    }
+
+    /**
+     * Validates configuration options.
+     */
+    var validateConfig = function(config) {
+        // Minimally validate all options.
+        // Format will be handled by the Redbot utility itself.
+        var severity = ['noFail', 'warning', 'bad'];
+        if (config.severity == undefined || severity.indexOf(config.severity) < 0 ) {
+            grunt.warning('Severity currently set to "' + config.severity + '" and must be set to a valid value: ' + severity.join(', '));
+        }
+        if (config.bin == undefined) {
+            grunt.warning('You have overridden the path to redbot with an invalid substitute.');
+        }
+        if (config.severity != 'noFail' && config.format != 'har') {
+            grunt.verbose.writeln('Severity option is skipped unless format is set to "har".'.yellow);
+        }
+    }
+
     grunt.registerMultiTask('redbot', 'Test HTTP implementation using redbot validator.', function() {
         var options = this.options({
             assets: false,
@@ -8,53 +38,33 @@ module.exports = function(grunt) {
             format: 'txt',
             severity: 'noFail'
         });
+        var config = targetConfigOverride(options, this.data);
+        validateConfig(config);
 
-        // Allow format and severity to be overridden per target.
-        if (this.data.format != undefined ) {
-            options.format = this.data.format;
-        }
-        if (this.data.severity != undefined) {
-            options.severity = this.data.severity;
-        }
-
-        // Minimally validate all options.
-        // Format will be handled by the Redbot utility itself.
-        var severity = ['noFail', 'warning', 'bad'];
-        if (options.severity == undefined || severity.indexOf(options.severity) < 0 ) {
-            grunt.warning('Severity currently set to "' + options.severity + '" and must be set to a valid value: ' + severity.join(', '));
-        }
-        if (options.bin == undefined) {
-            grunt.warning('You have overridden the path to redbot with an invalid substitute.');
-        }
         if (this.data.uri == undefined) {
             grunt.warning('You must specify at least one URI for redbot to process.');
         }
-        if (options.severity != 'noFail' && options.format != 'har') {
-            grunt.verbose.writeln('Severity option is skipped unless format is set to "har".'.yellow);
-        }
-
         // Post-process URIs
         if (!(this.data.uri instanceof Array)) {
             this.data.uri = [this.data.uri];
         }
-        
         var uris = this.data.uri;
+
         var done = this.async();
         var exec = require('child_process').exec;
-
         uris.forEach(function(uri) {
-            var args = [uri, '-o ' + options.format];
-            if (options.assets) {
+            var args = [uri, '-o ' + config.format];
+            if (config.assets) {
                 args.push('-a');
             }
-            var cmd = options.bin + ' ' + args.join(' ');
+            var cmd = config.bin + ' ' + args.join(' ');
             grunt.verbose.writeln(('Exec: ' + cmd).cyan);
             var res = exec(cmd, null, function(error, result, code) {
                 if (error) {
                     grunt.warn('exec error: ' + error)
                     return done(error);
                 }
-                if (options.format == 'har') {
+                if (config.format == 'har') {
                     result = JSON.parse(result);
                     var code = result.log.entries[0].response.status;
                     if (code >= 300) {
@@ -68,7 +78,7 @@ module.exports = function(grunt) {
                 // a configurable in case it needs to vary by project.
                 setTimeout(function() {
                     done();
-                }, options.timeout || 50);
+                }, config.timeout || 50);
             });
         });
     });
